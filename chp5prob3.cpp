4c730ba41099503f4cbc1e5307959f11a984c914
Marc Raymond <59947352+mkr123456@users.noreply.github.com>
2020-03-01 04:29:17 -0800

    Add files via upload

---
+++ chp5prob3.cpp
@@ -0,0 +1,41 @@
+// chp5prob2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
+int main ()
+{
+    cout << "\n Income versus GPA \n";
+    cout << "Name              \t Income   \t      GPA \n\n";
+    cout << "Jamie             \t 12300.00 \t      3.4   \n\n";
+    cout << "Linda             \t 14500    \t      3.92  \n\n";
+    cout << "Bob               \t  9400     \t      3.12  \n\n";
+    cout << "Marie             \t 15129.00 \t      4    \n\n\n";
+    cout << "** End of Report 1 **   \n\n";
+
+    cout << "\n Incom versus Age  \n";
+    cout << "Name              \t  Income     \t  Age \n\n";
+    cout << "Jamie             \t  12300.00   \t   19   \n\n";
+    cout << "Linda             \t  14500      \t   22   \n\n";
+    cout << "Bob               \t   9400       \t   21   \n\n";
+    cout << "Frank              \t  19129.00  \t   51   \n\n\n";
+    cout << "** End of Report 2 **   \n\n";
+
+    return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
